60f257e748cd9789e27aaa64a98414c864c22ade
Alejandro Garcia <alex-04@live.com>
2022-10-17 19:25:10 -0700

    Emailservice update

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -37,7 +37,7 @@ namespace ContactApp.Services
             try
             {
                 var host = _mailSettings.MailHost ?? Environment.GetEnvironmentVariable("MailHost");
-                var port = _mailSettings.MailPort !=0 ? _mailSettings.MailPort : int.Parse(Environment.GetEnvironmentVariable("Port"));
+                var port = _mailSettings.MailPort !=0 ? _mailSettings.MailPort : int.Parse(Environment.GetEnvironmentVariable("MailPort"));
                 var password = _mailSettings.MailPassword ?? Environment.GetEnvironmentVariable("MailPassword");
 
                 await smtpClient.ConnectAsync(host, port,SecureSocketOptions.StartTls);
